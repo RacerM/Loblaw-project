5e7a93bfdee08bed8c422d33628e184e767de9dd
Mengyi Shu <miashu22@gmail.com>
2023-03-31 11:17:53 -0400

    finish header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import "./Header.scss";
+import Headerlogo from "../../assets/Photos-PNG/Loblaws-Logo.png";
+import searchicon from "../../assets/Icons-SVG/Search Icon.svg";
+import cartlogo from "../../assets/Icons-SVG/Cart.svg";
+
+function Header() {
+  return (
+    <header className="header">
+      <img src={Headerlogo} className="headerlogo" alt="logo" />
+      <div id="input_container">
+        <input type="text" id="input" placeholder="search" />
+        <img src={searchicon} id="input_img" />
+      </div>
+      <img src={cartlogo} className="cartlogo" alt="logo" />
+      <button className="button" type="button">
+        Checkout
+      </button>
+    </header>
+  );
+}
+export default Header;
